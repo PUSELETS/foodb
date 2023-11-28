fe05016361f70f483a4c70a3a2961ae17f3ab4fd
PUSELETS <dimamabolo15@gmail.com>
2023-11-28 18:31:47 +0000

    first commit

---
+++ app/components/Navber.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navber() {
+  return (
+    <div className='w-3/5 h-screen bg-red-300'>
+        <ul className=''>
+            <li className=''>Burgger</li>
+            <li className=''>Spaghetti</li>
+            <li className=''>Pizza</li>
+            <li className=''>Hot-Dog</li>
+        </ul>
+    </div>
+  )
+}
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="flex flex-col h-screen">
+        <Head>
+          <title>Tweeter</title>
+        </Head>
+        <header className='flex items-center px-4 py-3 border-b'>
+          <p className='ml-6 text-lg font-extrabold'>
+            FOOD
+          </p>
+        </header>
+        <main className='flex-1'>
+          <section className={inter.className}>{children}</section>
+        </main>
+        <footer className='flex justify-center p-3 border-t relative'>
+          <div className='p-4 bg-gray-400 absolute bottom-16 right-6 rounded-full shadow'>
+            <div
+              className=' flex flex-col justify-between  w-6 h-5 sm:hidden' >
+              <span className='h-1 w-full bg-black rounded-lg'></span>
+              <span className='h-1 w-full bg-black rounded-lg'></span>
+              <span className='h-1 w-full bg-black rounded-lg'></span>
+            </div>
+          </div>
+          icon
+        </footer>
+      </body>
+
     </html>
   )
 }
