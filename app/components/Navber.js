5de4a46ed455be6dd63693f5d11d37d84b653f5a
PUSELETS <dimamabolo15@gmail.com>
2023-11-29 15:19:38 +0000

    y

--- app/components/Navber.js
+++ app/components/Navber.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import Link from 'next/link'
+import useMeasure from 'react-use-measure'
+import { MotionConfig, motion } from 'framer-motion'
 
 export default function Navber() {
+
+  const [ref, { height }] = useMeasure();
+
   return (
-    <div className='w-3/5 h-[100%] bg-red-300'>
-        <ul className=''>
-            <li className=''>Burgger</li>
-            <li className=''>Spaghetti</li>
-            <li className=''>Pizza</li>
-            <li className=''>Hot-Dog</li>
-        </ul>
-    </div>
+    <MotionConfig transition={{duration: 9}}>
+    <motion.div
+      animate= {{height}}
+      className='w-3/5 h-[100%] bg-red-300 overflow-hidden p-1 py-1'>
+      <ul 
+      ref={ref}
+      className=''>
+        <li className='list-none hover:bg-blue-200 text-center w-full rounded-lg '><Link className='no-underline text-black p-4 block' href={'/'}>Burger</Link></li>
+        <li className='list-none hover:bg-blue-200 text-center w-full rounded-lg '><Link className='no-underline text-black p-4 block' href={'/'}>Spaghetti</Link></li>
+        <li className='list-none hover:bg-blue-200 text-center w-full rounded-lg '><Link className='no-underline text-black p-4 block' href={'/'}>Pizza</Link></li>
+        <li className='list-none hover:bg-blue-200 text-center w-full rounded-lg '><Link className='no-underline text-black p-4 block' href={'/'}>Hot-Dog</Link></li>
+      </ul>
+    </motion.div>
+    </MotionConfig>
   )
 }
